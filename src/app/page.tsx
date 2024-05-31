444d444e937387f7d29df15163ced4f76b41caa0
archiash <46347714+archiash@users.noreply.github.com>
2024-05-31 18:23:12 +0700

    Remove Top NavBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,9 @@ import FloatingRect from "@/components/floatingRectangle"
 export default function Home() {
     return (
         <div className="flex flex-col h-screen w-full overflow-hidden">
-            <div className="flex bg-black h-12">
-                <div className="text-white w-3/4 text-xl text-left ml-10 items-center flex">
-                    Archiash
-                </div>  
-            </div>
             <div className="flex justify-center h-full items-center text-3xl">
-            <div className="flex items-center justify-center w-60 h-60 z-10">Pada Paradise</div>
-            <FloatingRect position={{x1:0, y1:-15, x2:0, y2:15}} size={{y:160}}></FloatingRect>
+            <div className="flex items-center justify-center px-4 py-2 z-10 border-2 border-black">Pada Paradise</div>
+            <FloatingRect position={{x1:0, y1:-30, x2:0, y2:30}} size={{y:160}}></FloatingRect>
             </div></div>
 
     );
