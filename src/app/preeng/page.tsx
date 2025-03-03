2ee7736dad1def6e50718f64db70337313da23ef
Archiash <archiash.legacy@gmail.com>
2025-03-03 12:07:14 +0700

    add preeng link

--- src/app/preeng/page.tsx
+++ src/app/preeng/page.tsx
@@ -2,10 +2,12 @@
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
-export default function() {
+export default function Page() {
 
     useEffect(() => {
         window.location.href = "https://www.figma.com/deck/RCpi04ziViEq5FHV5cGpzJ/Eigengrau?node-id=1-237&t=tsuSAQNUl1t5oUYw-1";
+        const router = useRouter()
+        router.push("https://www.figma.com/deck/RCpi04ziViEq5FHV5cGpzJ/Eigengrau?node-id=1-237&t=tsuSAQNUl1t5oUYw-1")
     }, [])
 
     return (
